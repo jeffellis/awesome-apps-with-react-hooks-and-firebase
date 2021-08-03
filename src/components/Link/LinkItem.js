d8615c7a951f25832465d739080eac2e8f1691c8
Jeff Ellis <jellis@rallydev.com>
2021-08-03 19:45:04 -0400

    Upvoting links, updating docs in firestore

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link, withRouter } from "react-router-dom";
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
 
-function LinkItem() {
-  return <div>LinkItem</div>;
+import { getDomain } from '../../utils';
+import { FirebaseContext } from '../../firebase/index'
+
+function LinkItem({ history, index, link, showCount }) {
+
+  const { firebase, user } = useContext(FirebaseContext);
+
+  function handleVote() {
+    if (!user) {
+      history.push('/login');
+      return;
+    }
+
+    const voteRef = firebase.db.collection('links').doc(link.id);
+    voteRef.get().then((doc) => {
+      if (doc.exists) {
+        const previousVotes = doc.data().votes;
+        const vote = {
+          votedBy: {
+            id: user.uid,
+            name: user.displayName,
+          }
+        }
+        const updatedVotes = [...previousVotes, vote];
+        voteRef.update({ votes: updatedVotes });
+      }
+    })
+  }
+
+  return (
+    <div className="flex items-start mt2">
+      <div className="flex items-center">
+        { showCount && <span className="gray">{index}</span> }
+        <div className="vote-button" onClick={handleVote}>&#9650;</div>
+      </div>
+      <div className="ml1">
+        <div>
+        {link.description} <span className="link">({getDomain(link.url)})&nbsp;</span>
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes by {link.postedBy.name} {distanceInWordsToNow(link.created)} {" | "}
+          <Link to={`/link/${link.id}`}>
+            { link.comments.length > 0 
+                ? `${link.comments.length} comments`
+                : "discuss"
+            }
+          </Link>
+        </div>
+      </div>  
+    </div>
+  );
 }
 
-export default LinkItem;
+export default withRouter(LinkItem);
