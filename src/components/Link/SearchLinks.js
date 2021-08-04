a8bbba73f7976833350456d10131fa785712400f
Jeff Ellis <jellis@rallydev.com>
2021-08-04 09:12:21 -0400

    client side search

--- src/components/Link/SearchLinks.js
+++ src/components/Link/SearchLinks.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import {FirebaseContext} from '../../firebase/index';
+import LinkItem from "./LinkItem";
 
 function SearchLinks() {
-  return <div>SearchLinks</div>;
+  const { firebase } = useContext(FirebaseContext);
+  const [filter, setFilter] = useState('');
+  const [links, setLinks] = useState([]);
+  const [filteredLinks, setFilteredLinks] = useState([]);
+
+  useEffect(() => {
+    getInitialLinks();
+  }, []);
+
+  function getInitialLinks() {
+    firebase.db.collection('links')
+      .get()
+      .then((snapshot) => {
+        const links = snapshot.docs.map( (doc) => ({ id: doc.id, ...doc.data() }) );
+        setLinks(links);
+        console.log('links: ', links);
+      });
+  }
+
+  function handleSearch(event) {
+    event.preventDefault();
+    const query = filter.toLowerCase();
+    const matchedLinks = links.filter((link) => {
+      return link.description.toLowerCase().includes(query) ||
+        link.url.toLowerCase().includes(query) ||
+        link.postedBy.name.toLowerCase().includes(query)
+    });
+
+    setFilteredLinks(matchedLinks);
+  };
+
+  return (
+    <div>
+    <form onSubmit={handleSearch}>
+      <div>
+        Search <input onChange={(event) => setFilter(event.target.value)}/>
+        <button>OK</button>
+      </div>
+    </form>
+
+    {
+      filteredLinks.map( (filteredLink, index) => {
+        return <LinkItem key={filteredLink.id} showCount={false} index={index} link={filteredLink}/>
+      })
+    }
+    </div>
+  );
 }
 
 export default SearchLinks;
